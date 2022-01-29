f895eac5540a2f0b674a15ed464e4e00d8f96fda
mauroch20 <56987870+mauroch20@users.noreply.github.com>
2022-01-29 16:26:13 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,64 +1,93 @@
 def call(List<String> paramsAllowedStage){
-	
-	paramsAllowedStage.each {
-                        println 'elementogradle:'+it
-          }
-   	println '------- test 1 ----------'
-	if (paramsAllowedStage.any{it=='build'})
-	{
-		println '------- test 2 ----------'
-		stage('Build & Unit Test'){
-			println '------- test 3 ----------'
+		
+		stage('Build & Unit Test')
+		{
+			println '****'+env.STAGE_NAME
+			if (paramsAllowedStage.any{it== env.STAGE_NAME})
+			{
+			
 				STAGE=env.STAGE_NAME
-				println '------- test 4 ----------'
 				sh 'env'
             			sh './gradlew clean build'
 				println "Stage: ${env.STAGE_NAME}"
+			}
+			else
+			{
+				println '------- SKIPPED build ----------'
+			}
 		}
 			
-	}
-	else
-	{
-		println '------- SKIPPED build ----------'
-	}
-	
-	stage('Sonar'){
-            		STAGE=env.STAGE_NAME
-            		def scannerHome = tool 'sonar-scanner';
-            		withSonarQubeEnv('sonar-server') {
-                	sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.sources=src"
-            		}
+		stage('Sonar'){
+			println '****'+env.STAGE_NAME
+			if (paramsAllowedStage.any{it== env.STAGE_NAME})
+			{
+				STAGE=env.STAGE_NAME
+				def scannerHome = tool 'sonar-scanner';
+				withSonarQubeEnv('sonar-server') {
+				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.sources=src"
+				}
+			}
+			else{
+				println '------- SKIPPED sonar ----------'
+			}
          	}
-	stage('Run'){
-		    	STAGE=env.STAGE_NAME
-			    println "Stage: ${env.STAGE_NAME}"
-            	sh "nohup bash gradlew bootRun & "
-            	sleep 80
+		
+		stage('Run'){
+			println '****'+env.STAGE_NAME
+			if (paramsAllowedStage.any{it==env.STAGE_NAME})
+			{
+		    		STAGE=env.STAGE_NAME
+				println "Stage: ${env.STAGE_NAME}"
+            			sh "nohup bash gradlew bootRun & "
+            			sleep 80
+			}
+			else
+			{
+				println '------- SKIPPED run ----------'
+			}
 			
 		}
-	stage('Test'){
-			STAGE=env.STAGE_NAME
-			println "Stage: ${env.STAGE_NAME}"
-            		sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-	}
-	stage('nexus') {
-            		STAGE=env.STAGE_NAME
-            		nexusPublisher nexusInstanceId: 'test-repo',
-            		nexusRepositoryId: 'test-repo',
-            		packages: [
-                	[
-                        	$class: 'MavenPackage',
-                        	mavenAssetList: [
-                            		[classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
-                        	],
-                        	mavenCoordinate: [
-                            		artifactId: 'DevOpsUsach2020',
-                            		groupId: 'com.devopsusach2020',
-                            		packaging: 'jar',
-                            		version: '0.0.1'
-                        	]
-                	]
-            		]
+	
+		stage('Test'){
+			println '****'+env.STAGE_NAME
+			if (paramsAllowedStage.any{it==env.STAGE_NAME})
+			{
+				STAGE=env.STAGE_NAME
+				println "Stage: ${env.STAGE_NAME}"
+            			sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+			}
+			else
+			{
+				println '------- SKIPPED test ----------'
+			}
+		}
+	
+		stage('nexus') {
+			println '****'+env.STAGE_NAME
+			if (paramsAllowedStage.any{it==env.STAGE_NAME})
+			{
+				STAGE=env.STAGE_NAME
+				nexusPublisher nexusInstanceId: 'test-repo',
+				nexusRepositoryId: 'test-repo',
+				packages: [
+				[
+					$class: 'MavenPackage',
+					mavenAssetList: [
+						[classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
+					],
+					mavenCoordinate: [
+						artifactId: 'DevOpsUsach2020',
+						groupId: 'com.devopsusach2020',
+						packaging: 'jar',
+						version: '0.0.1'
+					]
+				]
+				]
+			}
+			else
+			{
+				println '------- SKIPPED nexus ----------'
+			}
             
         }
         
