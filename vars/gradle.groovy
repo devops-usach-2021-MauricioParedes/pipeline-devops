a11c3fcbcb4e55fbb5f54468abc3ae36c1ef74d5
mauroch20 <56987870+mauroch20@users.noreply.github.com>
2022-01-29 09:23:06 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -13,44 +13,42 @@ def call(List<String> paramsAllowedStage){
 		}
 	
 		stage('Sonar'){
-            STAGE=env.STAGE_NAME
-            def scannerHome = tool 'sonar-scanner';
-            withSonarQubeEnv('sonar-server') {
-                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.sources=src"
-            }
-         
-		}
+            		STAGE=env.STAGE_NAME
+            		def scannerHome = tool 'sonar-scanner';
+            		withSonarQubeEnv('sonar-server') {
+                	sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.sources=src"
+            		}
+         	}
 		stage('Run'){
-		    STAGE=env.STAGE_NAME
+		    	STAGE=env.STAGE_NAME
 			println "Stage: ${env.STAGE_NAME}"
-            sh "nohup bash gradlew bootRun & "
-            sleep 80
+            		sh "nohup bash gradlew bootRun & "
+            		sleep 80
 			
 		}
 		stage('Test'){
 			STAGE=env.STAGE_NAME
 			println "Stage: ${env.STAGE_NAME}"
-            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-			
+            		sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
 		}
-	    stage('nexus') {
-            STAGE=env.STAGE_NAME
-            nexusPublisher nexusInstanceId: 'test-repo',
-            nexusRepositoryId: 'test-repo',
-            packages: [
-                [
-                        $class: 'MavenPackage',
-                        mavenAssetList: [
-                            [classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
-                        ],
-                        mavenCoordinate: [
-                            artifactId: 'DevOpsUsach2020',
-                            groupId: 'com.devopsusach2020',
-                            packaging: 'jar',
-                            version: '0.0.1'
-                        ]
-                ]
-            ]
+	    	stage('nexus') {
+            		STAGE=env.STAGE_NAME
+            		nexusPublisher nexusInstanceId: 'test-repo',
+            		nexusRepositoryId: 'test-repo',
+            		packages: [
+                	[
+                        	$class: 'MavenPackage',
+                        	mavenAssetList: [
+                            		[classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
+                        	],
+                        	mavenCoordinate: [
+                            		artifactId: 'DevOpsUsach2020',
+                            		groupId: 'com.devopsusach2020',
+                            		packaging: 'jar',
+                            		version: '0.0.1'
+                        	]
+                	]
+            		]
             
         }
         
