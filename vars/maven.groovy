4e32eaf1a6b54cdc67e389d060988750803384c4
mauroch20 <56987870+mauroch20@users.noreply.github.com>
2022-01-29 16:50:26 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,55 +1,74 @@
-def call(){
+def call(List<String> paramsAllowedStage){
   
-  stage("Compile"){ 
-      STAGE=env.STAGE_NAME
-      sh " ./mvnw clean compile -e"
+  stage(STAGE_BUILD){
+    if (paramsAllowedStage.any{it== env.STAGE_NAME})
+		{
+        STAGE=env.STAGE_NAME
+        sh " ./mvnw clean compile -e"
+        sh " ./mvnw clean test -e "
+        sh " ./mvnw clean package -e "
+        archiveArtifacts 'build/*.jar'
+        
+    }
+    else
+		{
+				println '------- SKIPPED'+STAGE_BUILD+' ----------'
+		}
      
 }
-stage('Sonar') { 
+stage(STAGE_SONAR) { 
+  if (paramsAllowedStage.any{it== env.STAGE_NAME})
+	{
       STAGE=env.STAGE_NAME
       def scannerHome = tool 'sonar-scanner'; 
       withSonarQubeEnv('sonar-server') {
         sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build "
       } 
-
-}
-
-stage("Test Code"){
-  STAGE=env.STAGE_NAME
-  sh " ./mvnw clean test -e "
-}
-stage("Jar"){ 
-  STAGE=env.STAGE_NAME
-  sh " ./mvnw clean package -e "
+  }
+  else
+  {
+				println '------- SKIPPED'+STAGE_SONAR+' ----------'
+	}
   
 }
-  
-stage('Guardando WAR') { 
+stage(STAGE_RUN){
+  if (paramsAllowedStage.any{it==env.STAGE_NAME})
+	{
     STAGE=env.STAGE_NAME
-    archiveArtifacts 'build/*.jar'
-  
+    sh "nohup bash mvnw spring-boot:run &" 
+    sleep 80
+  }
+  else
+	{
+				println '------- SKIPPED'+STAGE_RUN+' ----------'
+	}
+   
 }
- 
-stage("Nexus"){
-  
+stage(STAGE_TEST){
+  if (paramsAllowedStage.any{it==env.STAGE_NAME})
+	{
+    STAGE=env.STAGE_NAME
+    sh " curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing' "
+  }
+  else
+  {
+    println '------- SKIPPED'+STAGE_TEST+' ----------'
+  }
+}
+
+stage(STAGE_NEXUS){
+  if (paramsAllowedStage.any{it==env.STAGE_NAME})
+	{
     STAGE=env.STAGE_NAME
     nexusPublisher nexusInstanceId: 'test-repo', 
     nexusRepositoryId: 'test-repo', 
     packages: [[$class: 'MavenPackage', 
     mavenAssetList: [[classifier: '', extension: '', filePath: "${WORKSPACE}/build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]] }
-  
-
-stage("Run"){ 
-    STAGE=env.STAGE_NAME
-    sh "nohup bash mvnw spring-boot:run &" 
-    sleep 80
-   
-}
-stage("Testing Application"){ 
-    STAGE=env.STAGE_NAME
-    sh " curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing' "
-    
+  }
+  else
+  {
+    println '------- SKIPPED'+STAGE_NEXUS+' ----------'
+  }
 }
 
-}
 return this;
