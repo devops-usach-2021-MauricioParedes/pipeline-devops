27c1c2e7b3fca79b76c3e9374a30e6edb1ed5a5e
mauroch20 <56987870+mauroch20@users.noreply.github.com>
2022-01-30 10:41:31 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,73 +1,68 @@
 def call(List<String> paramsAllowedStage){
-		
+
+	if (paramsAllowedStage.any{it== STAGE_BUILD})
+	{
 		stage(STAGE_BUILD)
 		{
-			
-			if (paramsAllowedStage.any{it== env.STAGE_NAME})
-			{
-			
-				STAGE=env.STAGE_NAME
-				sh 'env'
-            			sh './gradlew clean build'
-				println "Stage: ${env.STAGE_NAME}"
-			}
-			else
-			{
-				println '------- SKIPPED'+STAGE_BUILD+' ----------'
-			}
+			STAGE=env.STAGE_NAME
+			sh 'env'
+            		sh './gradlew clean build'
+			println "Stage: ${env.STAGE_NAME}"
+					
 		}
-			
+	}
+	else
+	{
+		println '------- SKIPPED '+STAGE_SONAR+' ----------'
+	}
+	if (paramsAllowedStage.any{it== STAGE_SONAR})
+	{		
 		stage(STAGE_SONAR){
-			
-			if (paramsAllowedStage.any{it== env.STAGE_NAME})
-			{
-				STAGE=env.STAGE_NAME
-				def scannerHome = tool 'sonar-scanner';
-				withSonarQubeEnv('sonar-server') {
-				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.sources=src"
-				}
-			}
-			else{
-				println '------- SKIPPED'+STAGE_SONAR+' ----------'
-			}
+					
+			STAGE=env.STAGE_NAME
+			def scannerHome = tool 'sonar-scanner';
+			withSonarQubeEnv('sonar-server') {
+			sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.sources=src"
+
          	}
-		
+	}
+	else
+	{
+		println '------- SKIPPED '+STAGE_SONAR+' ----------'
+	}
+	if (paramsAllowedStage.any{it== STAGE_RUN})
+	{
 		stage(STAGE_RUN){
-			
-			if (paramsAllowedStage.any{it==env.STAGE_NAME})
-			{
-		    		STAGE=env.STAGE_NAME
-				println "Stage: ${env.STAGE_NAME}"
-            			sh "nohup bash gradlew bootRun & "
-            			sleep 80
-			}
-			else
-			{
-				println '------- SKIPPED'+STAGE_RUN+' ----------'
-			}
-			
+			STAGE=env.STAGE_NAME
+			println "Stage: ${env.STAGE_NAME}"
+            		sh "nohup bash gradlew bootRun & "
+            		sleep 80
 		}
-	
+						
+	}
+	else
+	{
+		println '------- SKIPPED '+STAGE_RUN+' ----------'
+	}
+	if (paramsAllowedStage.any{it==STAGE_TEST})
+	{
 		stage(STAGE_TEST){
-			
-			if (paramsAllowedStage.any{it==env.STAGE_NAME})
-			{
-				STAGE=env.STAGE_NAME
-				println "Stage: ${env.STAGE_NAME}"
-            			sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-			}
-			else
-			{
-				println '------- SKIPPED'+STAGE_TEST+' ----------'
-			}
+			STAGE=env.STAGE_NAME
+			println "Stage: ${env.STAGE_NAME}"
+            		sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
 		}
-	
-		stage(STAGE_NEXUS) {
 			
-			if (paramsAllowedStage.any{it==env.STAGE_NAME})
-			{
-				STAGE=env.STAGE_NAME
-				nexusPublisher nexusInstanceId: 'test-repo',
+	}
+	else
+	{
+		println '------- SKIPPED '+STAGE_TEST+' ----------'
+	}
+	if (paramsAllowedStage.any{it==STAGE_NEXUS})
+	{
+		stage(STAGE_NEXUS) {
+					
+			STAGE=env.STAGE_NAME
+			nexusPublisher nexusInstanceId: 'test-repo',
 				nexusRepositoryId: 'test-repo',
 				packages: [
 				[
@@ -83,13 +78,14 @@ def call(List<String> paramsAllowedStage){
 					]
 				]
 				]
-			}
-			else
-			{
-				println '------- SKIPPED'+STAGE_NEXUS+' ----------'
-			}
+		}
+	}
+	else
+	{
+		println '------- SKIPPED '+STAGE_NEXUS+' ----------'
+	}
             
-        }
+        
         
 }
 return this;
